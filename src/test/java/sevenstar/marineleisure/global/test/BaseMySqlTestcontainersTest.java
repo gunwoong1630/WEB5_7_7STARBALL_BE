da6af7cff65f772f0dc74bbf3a913f98d19b6ef6
gunwoong <gunwoong1630@gmail.com>
2025-07-16 20:38:20 +0900

    3

---
+++ src/test/java/sevenstar/marineleisure/global/test/BaseMySqlTestcontainersTest.java
@@ -0,0 +1,25 @@
+package sevenstar.marineleisure.global.test;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+public abstract class BaseMySqlTestcontainersTest {
+
+    @Container
+    protected static MySQLContainer<?> mysqlContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mysqlContainer::getUsername);
+        registry.add("spring.datasource.password", mysqlContainer::getPassword);
+    }
+}
